8c6ca02840eefe19df562f308c9ee993b4c253ef
suhasshettyy <scshetty07@gmail.com>
2021-07-26 13:18:53 +0530

    [Suhas] Add: NLogSubtraction

---
+++ NLogSubtraction/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Subtract sumproblem = new Subtract();
+            sumproblem.Subtraction(5,3);
+        }
+    }
+}
---
+++ NLogSubtraction/subtract.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sum
+{
+    class Subtract
+    {
+        static NLog nLog = new NLog();
+        public int Subtraction(int firstnumber, int secondnumber)
+        {
+            if (firstnumber == 0 || secondnumber == 0)
+            {
+                nLog.LogInfo("Subtraction method");
+                nLog.LogDebug("Debug Successfull");
+                nLog.LogWarn("First Number or Second Number is zero.");
+                return 0;
+            }
+            else
+            {
+                int result = firstnumber - secondnumber;
+                nLog.LogInfo("Subtraction method");
+                nLog.LogDebug("Debug Successfull");
+                nLog.LogWarn("Subtract Method Passed, Result : " + result);
+                return result;
+            }
+        
+        }
+    }
+}
